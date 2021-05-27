6a7f1a06948afbe5ad9dd3c4a963a7a808f226cd
Sam Carbine <scarbine@me.com>
2021-05-27 10:29:51 -0500

    Added js modules that automate fish creation

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+// Import the FishList function from the correct module
+import {FishList} from './FishList.js'
+
+
+/*
+    What is the CSS selector for the element where you
+    want to display the fish?
+
+    Use . for elements with a "class" attribute
+    Use # for elements with an "id" attribute
+ */
+const parentHTMLElement = document.querySelector("#fishListCard")
+
+
+parentHTMLElement.innerHTML = FishList()
